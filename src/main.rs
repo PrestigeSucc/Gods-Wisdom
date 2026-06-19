ecdb88785f5e651efcc1b1809e7581d0c94f4008
Parker Harris <parkerharris06@yahoo.com>
2026-06-19 03:36:31 -0500

    add visit and options
    Added question for where user wants to go and then gave user options

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    println!(
-        "Hello! This is the beginning of me and Parker's project for the Lord!
-Uhh"
-    );
+    println!("Hello! This is the beginning of me and Parker's project for the Lord!");
+
+    println!("What would you like to visit today?");
+    println!("[daily_bible_verse]   [bible_study]");
 }
