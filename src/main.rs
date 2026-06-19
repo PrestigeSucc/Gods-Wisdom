381e9daef8e1bc773d96b5b4547f3f89946fcbb1
Parker Harris <parkerharris06@yahoo.com>
2026-06-19 03:39:31 -0500

    add main menu title

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello! This is the beginning of me and Parker's project for the Lord!");
 
+    println!("Welcome To God's Wisdom!!!");
     println!("What would you like to visit today?");
     println!("[daily_bible_verse]   [bible_study]");
 }
