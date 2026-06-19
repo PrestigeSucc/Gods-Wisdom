5e04a2a174a04c5bbff37a59e6abf63993798954
Parker Harris <parkerharris06@yahoo.com>
2026-06-19 04:00:44 -0500

    add unfinished daily bible verse variable
    I need to figure out how to make the user gain access to a D.B.V. (Daily
    Bible Verse) page. The commented variable is a failed attempt.
    
    I also commented an idea for how the D.B.V. will function.

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     println!("Welcome To God's Wisdom!!!");
     println!("What would you like to visit today?");
     println!("[daily_bible_verse]   [bible_study]");
+
+    // let daily_bible_verse = println!(
+    //    "John 3:16 “For God so loved the world, that He gave His only begotten Son, that whoever believes in Him shall not perish, but have eternal life."
+    // );
+    // we can also add a rand expression to generate a random number, and have that number "match" with which ever number correlates to a verse in the database everyday.
 }
